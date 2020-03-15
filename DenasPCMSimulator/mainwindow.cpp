4f2629a1c8428543cc9e0e73693153ae2423a0a4
drantis <drantis@live.com>
2020-03-15 18:15:53 -0400

    Finished Main Menu. All buttons work. Check code comments for clarification

--- DenasPCMSimulator/mainwindow.cpp
+++ DenasPCMSimulator/mainwindow.cpp
@@ -1,14 +1,106 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
+
 {
     ui->setupUi(this);
+
+    //Setting the initial selection index to 0 and Battery to 100%
+    selectionIndex = 0;
+    batteryLevel = 100;
+    ui->batteryStatus->setValue(batteryLevel);
+
+    //Disabling the left and right buttons. Not needed in main menu
+    ui->leftButton->setEnabled(false);
+    ui->rightButton->setEnabled(false);
+
+    //Adding all the options to the view
+    for (int i =0; i < 6; i++){
+        ui->mainMenuOptions->addItem(allOptions[i]);
+    }
+
+    //Setting the cursor to the first item and highlitghting it
+    ui->mainMenuOptions->setCurrentRow(selectionIndex);
+    ui->mainMenuOptions->setFocus();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+//Up Button Clicked
+void MainWindow::on_upButton_clicked()
+{
+    if(selectionIndex == 0){
+        selectionIndex = 5;
+    } else {
+    selectionIndex -=1;
+    }
+    ui->mainMenuOptions->setCurrentRow(selectionIndex);
+    ui->mainMenuOptions->setFocus();
+}
+
+void MainWindow::on_downButton_clicked()
+{
+    if(selectionIndex == 5){
+        selectionIndex = 0;
+    } else {
+    selectionIndex +=1;
+    }
+    ui->mainMenuOptions->setCurrentRow(selectionIndex);
+    ui->mainMenuOptions->setFocus();
+}
+
+//Power Off Button
+void MainWindow::on_powerButton_clicked()
+{
+    exit(1);
+}
+
+
+//Select Button clicked
+void MainWindow::on_selectButton_clicked()
+{
+   QListWidgetItem *highlightedOption =  ui->mainMenuOptions->item(selectionIndex);
+   changeMenu(highlightedOption->text());
+}
+
+////Change Menu
+void MainWindow::changeMenu(QString selectedMenu)
+{
+    if (selectedMenu.contains(allOptions[0])){
+        //CREATE INSTANCE OF PROGRAM MENU obj
+    }
+    else if (selectedMenu.contains(allOptions[1])){
+        //CREATE INSTANCE OF Frequency MENU obj
+    }
+    else if (selectedMenu.contains(allOptions[2])){
+        //CREATE INSTANCE OF MED MENU obj
+    }
+    else if (selectedMenu.contains(allOptions[3])){
+        //CREATE INSTANCE OF Screening MENU obj
+    }
+    else if (selectedMenu.contains(allOptions[4])){
+        //CREATE INSTANCE OF Children MENU obj
+    }
+    else if (selectedMenu.contains(allOptions[5])){
+        //CREATE INSTANCE OF Settings MENU obj
+    }
+    return;
+}
+
+//Change the battery level
+void MainWindow::setBatteryLevel(int newBatteryLevel){
+    batteryLevel = newBatteryLevel;
+}
+
+//Get the current battery level
+int MainWindow::getBatteryLevel(){
+    return batteryLevel;
+}
+
--- DenasPCMSimulator/mainwindow.h
+++ DenasPCMSimulator/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+using namespace std;
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,40 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_upButton_clicked();
+
+    void on_powerButton_clicked();
+
+    void on_downButton_clicked();
+
+    void on_selectButton_clicked();
+
+private:
+    void changeMenu(QString selectedMenu);
+
+public:
+    int getBatteryLevel();
+
+public:
+    void setBatteryLevel(int newBatteryLevel);
+
 private:
     Ui::MainWindow *ui;
+
+private:
+    int batteryLevel;
+
+private:
+    QString allOptions [6] = {"PROGRAM","FREQUENCY","MED","SCREENING","CHILDREN","SETTINGS"};
+
+private:
+    int selectionIndex;
+
 };
+
+
+
+
 
 #endif // MAINWINDOW_H
